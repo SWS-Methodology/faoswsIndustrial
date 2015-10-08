28ef3866b24b88202e4c9065fa72e5ec303e4b33
Bruno Vidigal <bruno.caetanovidigal@fao.org>
2015-10-08 16:05:29 +0200

    R codes for industrial module

---
+++ R/getCountryCodeSUA.R
@@ -0,0 +1,13 @@
+##' Get Country Code SUA
+##' 
+##' @param country_name The name of the country.
+##' 
+##' @return Codes
+##'
+
+
+getCountryCodeSUA = function(country_name){
+map = GetCodeList("agriculture", "agriculture", "geographicAreaM49")
+  return(map$code[map$description == country])
+}
+
