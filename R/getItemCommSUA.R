02ba9948a37ebb9d764be8e073e7a09dae28cb0c
Bruno Vidigal <bruno.caetanovidigal@fao.org>
2015-12-01 16:33:07 +0100

    Changed the output from commodities names to item CPC code

--- R/getItemCommSUA.R
+++ R/getItemCommSUA.R
@@ -1,14 +1,18 @@
 ##' Get Item Comm SUA
 ##' 
-##' @param Commodity_Description The description of the commodity.
+##' @param Measured_Item_Psd The USDA item code .
 ##' 
 ##' @return Codes
 ##'
 
-getItemCommSUA = function(Commodity_Description){
-  map = read.csv("~/Github/faoswsIndustrial/Data/industrialUseCommodityMap.csv",
+getItemCommSUA = function(Measured_Item_Psd){
+  map = fread("~/Github/faoswsIndustrial/Data/industrialUseCommodityMap.csv",
                  stringsAsFactors = FALSE)
-  return(map$measuredItemCPC[map$Commodities == Commodity_Description])
+  result = merge(map, data.table(measuredItemPsd = as.numeric(Measured_Item_Psd), index = 1:length(Measured_Item_Psd)), 
+                 by="measuredItemPsd", all.y=T)
+  setkeyv(result, "measuredItemPsd")
+  result <- result[order(result$index)]
+  result[, measuredItemCPC]
 }
 
 
