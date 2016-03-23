435f9bcfced469026312ef344856625f09d90455
Bruno Vidigal <bruno.caetanovidigal@fao.org>
2016-03-23 20:32:36 +0100

    Added more analyses

--- sandbox/analyseEuroStatData.R
+++ sandbox/analyseEuroStatData.R
@@ -131,7 +131,6 @@ if(swsContext.computationParams$yearToProcess <= 2011)
 
 ## This code below is pulling data from the old system for "Other util"
 
-
 # 51 production (Mt)
 # 61 imports (Mt)
 # 71 stock variation (Mt) 
@@ -147,7 +146,7 @@ elements = c("51", "61", "71", "91", "101", "131", "141", "151")
 # commodities used to produce it. The names of the commodities are: "Oil palm fruit",
 # "Palm oil", "Oil of Palm Kernel", "Cake of  Palm kernel", "Rapeseed or canola oil, crude",
 # "Cake of  Rapeseed", "Soya bean oil", "Cake of  Soya beans", "Sunflower-seed oil, crude",
-# "Cake of  Sunflower seed", ""
+# "Cake of  Sunflower seed"
 
 items = c("254", "257", "258", "259", "271", "272", "237", "238", "268", "269")
 
@@ -159,9 +158,7 @@ key = DatasetKey(domain = "faostat_one", dataset = "FS1_SUA_UPD", dimensions = l
 )
 data = GetData(key)
 
-
-#initialData = copy(data)
-
+# dcast.data.table
 data <- dcast.data.table(data,
                          geographicAreaFS + measuredItemFS + timePointYears ~ measuredElementFS,
                          value.var = "Value")
@@ -178,6 +175,7 @@ data[, measuredItemFS := formatC(as.numeric(measuredItemFS), width = 4,
 data[, measuredItemCPC := fcl2cpc(as.character(measuredItemFS))]
 data
 
+# names of the commodities and countries
 data <- nameData("agriculture", "aproduction", data)
 
 data[grepl("palm", tolower(measuredItemCPC_description))][, .N, c("measuredItemCPC_description", "measuredItemFS")]
@@ -198,7 +196,7 @@ data[grepl("sunflower", tolower(measuredItemCPC_description)), commodity := "Sun
 data[, .N, commodity]
 data[geographicAreaM49 == "250"]
 
-
+# no NA's
 for(cname in c("feed", "processed", "food", "otherUtil", "production", 
                "imports", "stockVariation", "exports")){
   data[is.na(get(cname)), c(cname) := 0]
@@ -220,7 +218,6 @@ europeanCountryCode[, geographicAreaM49 :=  as.factor(countrycode(countryName, "
 europeanCountryCode = europeanCountryCode[flagEU28 == 1]
 europeanCountryCode
 
-
 # merge data with the the flag of EU28
 
 class(data$geographicAreaM49)
@@ -229,26 +226,55 @@ class(europeanCountryCode$geographicAreaM49)
 data = merge(data, europeanCountryCode[, c("geographicAreaM49", "flagEU28"), with = F],
       by = "geographicAreaM49", all.x=T)
 
-data[, flagEU28 := as.factor(flagEU28)]
-
 data[is.na(flagEU28), flagEU28 := 0]
+
+data[, flagEU28 := as.factor(flagEU28)]
 
 data[, .N, flagEU28]
 
-tab1 = data[, list(prodNetTrade = sum(prodNetTrade)),
+# by country, flagEU28 and commodity
+countryByCommodity = data[, list(prodNetTrade = sum(prodNetTrade)),
      by=list(geographicAreaM49, geographicAreaM49_description, flagEU28, commodity)]
 
-tab1[geographicAreaM49 == "250"]
+countryByCommodity[geographicAreaM49 == "250"]
 
-tab1[, percentByCountry := prodNetTrade/sum(prodNetTrade), 
+countryByCommodity[, biofuelStructByCountry := prodNetTrade/sum(prodNetTrade), 
      by = geographicAreaM49]
 
-
-tab2 = data[, list(prodNetTrade = sum(prodNetTrade)),
+# by flagEU28 and commodity
+flagEU28Commodity = data[, list(prodNetTrade = sum(prodNetTrade)),
      by=list(flagEU28, commodity)]
 
-tab2[, percentByCountry := prodNetTrade/sum(prodNetTrade), 
+flagEU28Commodity[, biofuelStructEU28 := prodNetTrade/sum(prodNetTrade), 
      by = flagEU28]
+
+
+# Now, we have to merge the table countryByCommodity with  the table 
+# euroStatBiofuel.
+
+keys = c("geographicAreaM49", "commodity")
+
+setnames(euroStatBiofuel, "m49", "geographicAreaM49")
+class(euroStatBiofuel$geographicAreaM49)
+euroStatBiofuel[, geographicAreaM49 := as.factor(geographicAreaM49)]
+class(countryByCommodity$geographicAreaM49)
+
+euroStatBiofuel = merge(euroStatBiofuel, countryByCommodity[, c("geographicAreaM49", "commodity",
+                                              "biofuelStructByCountry"), with = F],
+      by=keys, all.x=T)
+
+# Now, we can apply the same rules that we applied before.
+
+euroStatBiofuel[geographicAreaM49 == "250" & year == 2003 & biofuel == "biodiesel"]
+
+euroStatBiofuel[biofuel == "biodiesel", valueBiodiselByCommodityTonnes := value * biofuelStructByCountry * 1000]
+
+# we need to convert from litres to tonnes
+
+## Now we can calculate how many tonnes of each commodity was used
+euroStatBiofuel[biofuel == "biodiesel", prodBiodiselByCommodity := valueBiodiselByCommodityTonnes/conversion_litres_tonne]
+euroStatBiofuel
+
 
 ## Comparing data
 
