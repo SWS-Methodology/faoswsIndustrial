ecdda825bdce0f039729a8b754a0ef6df3f2b7f2
Bruno Vidigal <bruno.caetanovidigal@fao.org>
2016-04-11 14:26:20 +0200

    Add countrycode function from countrycode package

--- R/getCountryCodeSUA.R
+++ R/getCountryCodeSUA.R
@@ -1,17 +1,19 @@
 ##' Get Country Code SUA
-##' 
+##'
 ##' @param country_name The name of the country.
-##' 
+##'
 ##' @return Codes
 ##'
 ##' @export
-##' 
+##'
+##' @import countrycode
+##'
 
 
 getCountryCodeSUA = function(country_name){
-map = fread("~/Github/faoswsIndustrial/Data/usdaMap.csv",
-                 stringsAsFactors = FALSE, na.strings = "")
-result = merge(map, data.table(usdaCode = country_name, index = 1:length(country_name)), 
+map = fread("Data/countriesUSDA.csv")
+map[, m49 := countrycode::countrycode(usdaCode, "fips104", "un")]
+result = merge(map, data.table(usdaCode = country_name, index = 1:length(country_name)),
                by="usdaCode", all.y=T)
 setkeyv(result, "usdaCode")
 result <- result[order(result$index)]
