fdd94c8a73f3afcd6786a7f080d406655859563d
CaetanoVidigal <Bruno.CaetanoVidigal@fao.org>
2018-02-01 15:06:53 +0100

    Integrating questionnaire data and USDA data.

--- modules/impute_industrial/main.R
+++ modules/impute_industrial/main.R
@@ -36,8 +36,35 @@ if(CheckDebug()){
 files = dir("R", full.names = TRUE)
 sapply(files, source)
 
+###################################################################################################
+## This chunck of code should be delete after Daniele moves the element 5153 to 5165 from 1961-2011
+area <- GetCodeList("agriculture", "aproduction", "geographicAreaM49")[, code]
+items <- GetCodeList("agriculture", "aproduction", "measuredItemCPC")[, code]
+elements <- c("5153", "5165")
+years <- as.integer(unique(GetCodeList("agriculture", "aproduction", "timePointYears")[, code]))
+years <- as.character(years[years >= 1961L])
+
+raw_data_key <- DatasetKey(domain = "agriculture",
+                           dataset = "aproduction",
+                           dimensions = list(
+                             geographicAreaM49 = Dimension(name = "geographicAreaM49", keys = area),
+                             measuredElement = Dimension(name = "measuredElement", keys = elements),
+                             measuredItemCPC = Dimension(name = "measuredItemCPC", keys = items),
+                             timePointYears = Dimension(name = "timePointYears", keys = years)
+                           )
+)
+
+raw_data <- GetData(raw_data_key)
+raw_data[timePointYears < 2012, measuredElement := "5165"]
+raw_data <- raw_data[!(measuredElement == 5153)]
+
+## Let's exclude 0 (M, -)
+raw_data <- raw_data[!(flagObservationStatus == "M" & flagMethod == "-" & Value == 0)]
+
+###################################################################################################
+
 ## Extracting data from USDA domain/dataset
-yearRange <- as.character(1961:2015)
+yearRange <- as.character(1961:2016)
 vegetableOilsDataForIndUses <- getIndustrialData(yearRange)
 
 industrialUsesData <- splitIndustrialData(vegetableOilsDataForIndUses)
@@ -50,12 +77,32 @@ industrialUsesData[group == "estimated", flagMethod := "e"]
 industrialUsesData[, group := NULL]
 
 
-setcolorder(industrialUsesData, c("timePointYears", "geographicAreaM49",
-                                  "measuredItemCPC", "measuredElement", "Value",
+setcolorder(industrialUsesData, c("geographicAreaM49", "measuredElement",
+                                  "measuredItemCPC", "timePointYears", "Value",
                                   "flagObservationStatus", "flagMethod"))
+
+setnames(industrialUsesData, "Value", "value_usda")
+setnames(industrialUsesData, "flagObservationStatus", "flagObservationStatus_usda")
+setnames(industrialUsesData, "flagMethod", "flagMethod_usda")
+
+## We will merge the data coming from the questionnaire with the data from USDA.
+## If there's data for the same cpc in both datasets, we will use data from quest.
+
+merge_data <- merge(raw_data, industrialUsesData,
+  by = c("geographicAreaM49", "measuredElement", "measuredItemCPC", "timePointYears"),
+  all = T)
+
+merge_data <- merge_data[!is.na(geographicAreaM49)]
+merge_data[is.na(Value), `:=` (Value = value_usda,
+                               flagObservationStatus = flagObservationStatus_usda,
+                               flagMethod = flagMethod_usda)]
+
+# merge_data[geographicAreaM49 == 120 & measuredItemCPC == "21691.14" & timePointYears == 2012]
+merge_data[, c("value_usda", "flagObservationStatus_usda", "flagMethod_usda") := NULL]
+
 ## Save data
-stats <- SaveData(domain = "agriculture", dataset = "aproduction",
-                 data = industrialUsesData)
+stats <- SaveData(domain = "industrialUse", dataset = "industrialusedata",
+                 data = merge_data)
 
 paste0(stats$inserted, " observations written, ",
        stats$ignored, " weren't updated, ",
